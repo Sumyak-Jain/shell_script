03f0c60fa0a1a9cf37c39a07cac88318a11827bc
SUMYAK JAIN <3456gaurav@gmail.com>
2019-10-13 16:53:16 +0530

    Create zip_labperiod.sh
    Q. make a schedule on shell files to zip particular period

---
+++ zip_labperiod.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+read -e -p "enter loc for deleting files:" loc
+read -e -p "enter the days " day
+cd $loc
+rm $(find -mtime +$day  -name "*.tar")
+rm $(find -mtime +$day  -name "*.tar.gz")
