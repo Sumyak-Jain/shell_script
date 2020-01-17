ecb5500b57267379771fabbe6eb4aae34f958383
SUMYAK JAIN <3456gaurav@gmail.com>
2020-01-17 11:27:57 +0530

    conditional

---
+++ conditional.sh
@@ -0,0 +1,37 @@
+//shell program for making use of loop,if-else, and switch case in a single scipt
+
+#/bin/bash
+echo "enter your name?"
+read name 
+echo "hello, $name"
+read -p "Enter an integer: " int1    
+echo " printing all numbers from $int1 till greater then 0"
+for((count=$int1;count>0;count--)) //using for loop
+do
+echo  "$count"
+done
+printf "\n"
+ if [ $int1 -eq 0 ] //checking if else
+ then 
+     echo "Zero" 
+ elif [ $int1 -lt 0 ] 
+ then 
+     echo "Negative number" 
+ else 
+     if [ $((int1%2)) -eq 0 ] 
+     then 
+         echo " it is a even number" 
+     else 
+         echo " it is a Odd number" 
+     fi 
+ fi
+echo " enter a fruit name  to know its rate : apple,banana,kiwi" //using switch case
+read FRUIT
+case "$FRUIT" in
+   "apple") echo "Apple is Rs 120/kg." 
+   ;;
+   "banana") echo "banana is Rs 50/dozen." 
+   ;;
+   "kiwi") echo "kiwi is Rs50 for 4." 
+   ;;
+esac
